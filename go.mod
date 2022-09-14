df6cd9420445fc31fd7c3015e12ecfe1dd3a93a9
Eslam-Nawara <eslamnawara44@gmail.com>
2022-09-14 18:10:13 +0200

    Check all links

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module linkchecker
 
 go 1.19
 
-require github.com/pelletier/go-toml v1.9.5
+require (
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/pelletier/go-toml v1.9.5
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20220909164309-bea034e7d591 // indirect
+)
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"reflect"
 	"strings"
 
 	toml "github.com/pelletier/go-toml"
+	"golang.org/x/net/html"
 )
 
 func main() {
@@ -21,32 +23,50 @@ func main() {
 	}
 
 	mp := config.ToMap()
-	brokenLinks := checkMap(mp)
 
-	for _, elem := range brokenLinks {
-		fmt.Println(elem)
-	}
+	checkMap(mp)
 
 }
 
-func checkMap(mp any) []string {
-	brokenLinks := []string{}
+func checkMap(mp any) {
 	for _, val := range mp.(map[string]any) {
 
 		if reflect.TypeOf(val).Kind() == reflect.Map {
-			brokenLinks = append(brokenLinks, checkMap(val)...)
-
+			checkMap(val)
 		} else {
-
-			if !validLink(val.(string)) {
-				brokenLinks = append(brokenLinks, val.(string))
-			}
+			ch := make(chan bool)
+			go validateWabsite(val.(string), ch)
+			<-ch
 		}
 	}
-	return brokenLinks
 }
 
-func validLink(link string) bool {
+func validateWabsite(host string, ch chan bool) {
+	if !strings.HasPrefix(host, "http://") && !strings.HasPrefix(host, "https://") {
+		host = fmt.Sprintf("http://%s", host)
+	}
+	resp, err := http.Get(host)
+
+	if err != nil {
+		fmt.Println(host)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	links := getLinks(resp.Body)
+
+	for _, link := range links {
+		if !validateLink(link) {
+			fmt.Println("failed: ", link)
+		} else {
+			fmt.Println("passed: ", link)
+		}
+	}
+	ch <- true
+}
+
+func validateLink(link string) bool {
 	if !strings.HasPrefix(link, "http://") && !strings.HasPrefix(link, "https://") {
 		link = fmt.Sprintf("http://%s", link)
 	}
@@ -65,4 +85,28 @@ func validLink(link string) bool {
 	headStatusCode := headResp.StatusCode
 	return (getStatusCode >= 200 && getStatusCode < 400) ||
 		(headStatusCode >= 200 && headStatusCode < 400)
+}
+
+func getLinks(body io.Reader) []string {
+	var links []string
+	z := html.NewTokenizer(body)
+	for {
+		tt := z.Next()
+
+		switch tt {
+		case html.ErrorToken:
+			return links
+		case html.StartTagToken, html.EndTagToken:
+			token := z.Token()
+			if "a" == token.Data {
+				for _, attr := range token.Attr {
+					if attr.Key == "href" {
+						links = append(links, attr.Val)
+					}
+
+				}
+			}
+
+		}
+	}
 }
